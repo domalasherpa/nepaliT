68fe53cdcf0fd42230cd7f1a875fd63123bc0a59
Domala Sherpa <domalasherpa9813@gmail.com>
2024-05-23 08:07:19 +0545

    minor refactor of code

--- wordsGenerator.js
+++ wordsGenerator.js
@@ -1,8 +1,10 @@
+let randomWords = [];
 
 async function getRandomWords(){
     try{
         let randomWords = await fetch('http://localhost:7071/api/httpTrigger1');
         randomWords = await randomWords.json();
+        console.log(randomWords);
         return randomWords["randomWords"];
     }
     catch(error){
@@ -11,24 +13,27 @@ async function getRandomWords(){
     }
 }
 
-export async function showRandomWords(){
-    let randomWords = await getRandomWords();
-    const wordDisplay = document.querySelector(".wordDisplay");
-    const randomWord= document.createElement('p');
-    const charDisplay = document.createElement("p");
-    
-    let word = randomWords[0];
-    randomWord.innerText = word;
-    randomWord.classList.add("randomWord");
-    charDisplay.classList.add("charDisplay");
+export async function loadRandomWords(){
+    //use local storage or session storage to reduce api hit 
+    if(randomWords.length == 0){
+        randomWords = await getRandomWords();
+    }
+    console.log(randomWords);
+    let randomWord = randomWords.pop();
+    return randomWord;
+}
 
-    wordDisplay.appendChild(randomWord);
+export function displayWord(word){
+    const randomWord= document.querySelector(".randomWord");
+    const charDisplay = document.querySelector(".charDisplay");
+    
+    randomWord.innerText = word;
+
+    charDisplay.innerHTML = ""; //remove the previous chars
     for(let i = 0; i < word.length; i++){
         let singleRandomChar = document.createElement("span");
         singleRandomChar.innerText = word[i];
         charDisplay.appendChild(singleRandomChar);
     };
 
-    wordDisplay.appendChild(charDisplay);
-    wordDisplay.classList.add("arya-regular");
 }
